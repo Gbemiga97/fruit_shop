951617e84ebe108b5ba43fc3e7789ddb6409a578
Gbemiga Salam <holuwagbemiga97@gmail.com>
2024-03-02 15:58:20 +0100

    initial commit

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,19 +1,55 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import tw from 'twrnc'
+import { Bars3CenterLeftIcon, ShoppingCartIcon } from 'react-native-heroicons/solid'
+import { data } from '../utils'
+
 
 export default function HomeScreen() {
+
+    const [activeCategory, setActiveCategory] = useState("Oranges")
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={tw`flex-1 bg-orange-50 pt-6`}>
         {/* top bar */}
-        <View>
-           
+        <View style={tw`mx-5 flex-row justify-between items-center`}>  
+        <Bars3CenterLeftIcon size="30" color="black" />        
         <TouchableOpacity 
-        style={tw-`p-2 rounded-xl bg-orange-100`}
+        style={tw`p-2 rounded-xl bg-orange-100`}
         >
-            <Text>Home Screen here</Text>
+            <ShoppingCartIcon size='25' color='orange' />
         </TouchableOpacity>
+        </View>
+
+        {/* categories */}
+        <View style={tw`mt-4`}>
+            <Text style={tw`text-2xl tracking-widest ml-5 font-medium text-gray-500`}>
+                Seasonal
+            </Text>
+            <Text style={tw`text-3xl font-semibold ml-5 text-gray-500`}>
+                Fruits and Vegetable
+            </Text>
+            <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            style={tw`mt-8 px-5`}
+            >
+                {
+                    data.categories.map((category, i) => (
+                        <TouchableOpacity style={tw`mr-8 relative`} onPress={() => setActiveCategory(category)}>
+                            <Text style={tw`text-xl text-gray-500 ${category === activeCategory ? 'font-bold' : ''}`}>
+                                {category}
+                            </Text>
+                            {
+                                category === activeCategory ? (
+                                    <Text style={tw`text-orange-400 font-extrabold -mt-3 ml-2`}>__ _</Text>
+                                ) : null
+                            }
+                        </TouchableOpacity>
+                    ))
+                }
+            </ScrollView>
         </View>
     </SafeAreaView>
   )
---
+++ utils/data.js
@@ -0,0 +1,63 @@
+const categories = [
+    "Oranges",
+    "Bananas",
+    "Pineapple",
+    "Apple",
+    "Cherry"
+]
+
+const featuredFruits = [
+    {
+        name: 'Australian Orange',
+        price: "12.30",
+        stars: 4,
+        desc: "sweet and juicy",
+        shadow: "orange",
+        img: require(`../assets/images/orange.png`),
+        color: opacity => `rgba(251, 216, 146, ${opacity})`
+    },
+    {
+        name: 'Flesh Nectari',
+        price: "12",
+        stars: 3,
+        desc: "Sweet and juicy",
+        shadow: "red",
+        img: require(`../assets/images/peach.png`),
+        color: opacity => `rgba(255, 170, 157) ${opacity}`
+    },
+    {
+        name: 'Black Grapes',
+        price: "40",
+        stars: 4,
+        desc: "Sweet and juicy",
+        shadow: "purple",
+        img: require(`../assets/images/grapes.png`),
+        color: opacity => `rgba(214, 195, 246, ${opacity})`
+    },
+    {
+        name: 'Red Grapefruit',
+        price: "30",
+        stars: 4,
+        desc: "Sweet and juicy",
+        shadow: "red",
+        img: require(`../assets/images/red_orange.png`),
+        color: opacity => `rgba(255, 163, 120, ${opacity})`
+    },
+    {
+        name: 'Green Apple',
+        price: "10.5",
+        stars: 4,
+        desc: "Sweet and juicy",
+        shadow: "green",
+        img: require(`../assets/images/green_apple.png`),
+        color: opacity => `rgba(139, 243, 139, ${opacity})`
+    },
+]
+
+
+
+const data = {
+    categories
+}
+
+export default data
